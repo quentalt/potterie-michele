5f3b7fb1bf5c418331f451eb1ed4b9156beda5f9
quentalt <quentin.altieri@gmail.com>
2026-02-18 18:18:54 +0100

    connect to prisma database

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,10 @@
 import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@prisma/client'
-import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from '@/lib/generated/prisma/client'
 
 
 const connectionString = `${process.env.DATABASE_URL}`
 
-const adapter = new PrismaNeon({ connectionString })
-export const prisma = new PrismaClient({ adapter })
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
+export default prisma;
