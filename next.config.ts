141634ca51a89d0ee2a51dec41cd3f58b3aacfe2
quentalt <quentin.altieri@gmail.com>
2026-02-18 19:01:53 +0100

    add hostname ogresdelaterre.fr for fetch images

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
     images: {
         remotePatterns: [
             {
-                hostname: "images.selency.com",
+                hostname: "ogresdelaterre.fr",
             },
         ],
     }
