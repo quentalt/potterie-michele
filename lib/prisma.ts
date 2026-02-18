1c9525f3e4b6c97261eab422e4225a0894f54955
quentalt <quentin.altieri@gmail.com>
2026-02-18 23:30:14 +0100

    add singleton to prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,12 @@
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '@/lib/generated/prisma/client'
+import { PrismaClient } from "@prisma/client";
 
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const connectionString = `${process.env.DATABASE_URL}`
+export const prisma =
+    globalForPrisma.prisma || new PrismaClient();
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
